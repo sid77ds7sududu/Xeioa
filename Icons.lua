e6caaac2bb7d90faf0ae19de4b0c1601055ab583
sid77ds7sududu <am9578661111@gmail.com>
2026-06-06 08:01:30 +0200

    Create Icons.lua

---
+++ Icons.lua
@@ -0,0 +1,38 @@
+local Icons = {
+    House = { IconId = "rbxassetid://10734882173" },
+    Settings = { IconId = "rbxassetid://10734950309" },
+    Profile = { IconId = "rbxassetid://10723414172" },
+    Search = { IconId = "rbxassetid://10734945412" },
+    Menu = { IconId = "rbxassetid://10723345423" },
+
+    Sword = { IconId = "rbxassetid://10734951437" },
+    Gun = { IconId = "rbxassetid://13834070251" },     -- Custom/Standard-Waffen-Icon
+    Weapon = { IconId = "rbxassetid://10734951437" },  -- Alternativ auch Sword
+    Shield = { IconId = "rbxassetid://10734947231" },
+    Crosshair = { IconId = "rbxassetid://10723348128" },
+
+    Eye = { IconId = "rbxassetid://10723351944" },
+    User = { IconId = "rbxassetid://10723414172" },
+    Backpack = { IconId = "rbxassetid://10723315750" },
+    Inventory = { IconId = "rbxassetid://10723374246" }, -- Koffer/Paket-Icon
+    Friends = { IconId = "rbxassetid://10723413551" },
+
+    Coin = { IconId = "rbxassetid://10734898114" },    -- Dollar/Münzen-Symbol
+    Gem = { IconId = "rbxassetid://10734896791" },     -- Diamant-Form
+    Shop = { IconId = "rbxassetid://10734948432" },    -- Einkaufswagen
+    Gift = { IconId = "rbxassetid://10733153573" },
+    Trophy = { IconId = "rbxassetid://10734953716" },
+
+    Star = { IconId = "rbxassetid://10734949856" },
+    Heart = { IconId = "rbxassetid://10723363363" },
+    Bell = { IconId = "rbxassetid://10723318237" },
+    Mail = { IconId = "rbxassetid://10734900767" },
+    Lock = { IconId = "rbxassetid://10734901633" },
+    Unlock = { IconId = "rbxassetid://10734954316" },
+    Warning = { IconId = "rbxassetid://10723415181" },
+    Fire = { IconId = "rbxassetid://10723354325" },
+    Lightning = { IconId = "rbxassetid://10734901140" },
+    Crown = { IconId = "rbxassetid://10734899557" }
+}
+
+return Icons
